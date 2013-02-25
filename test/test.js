8df3da120e52ed56c9cabbbef125266f7275fee1
Mikko Koski <mikkokos@gmail.com>
2013-02-25 09:50:12 +0200

    Validators to own file

--- test/test.js
+++ test/test.js
@@ -4,58 +4,59 @@ var expect = require("expect.js");
 var key = require("../lib/keyword");
 
 describe('argument validations', function(){
+  var validators = require("../lib/validators");
   describe('#suite()', function(){
     it('should throw for illegal arguments', function(){
-      expect(key.validation.isSuite([{}])).to.be.ok();
-      expect(key.validation.isSuite([["invalid"]])).to.not.be.ok();
-      expect(key.validation.isSuite([function() { /* invalid */}])).to.not.be.ok();
+      expect(validators.isSuite([{}])).to.be.ok();
+      expect(validators.isSuite([["invalid"]])).to.not.be.ok();
+      expect(validators.isSuite([function() { /* invalid */}])).to.not.be.ok();
     });
   });
   describe('#def()', function(){
     it('should throw for illegal arguments', function(){
-      expect(key.validation.isDef(["name", function() {}])).to.be.ok();
-      expect(key.validation.isDef([["invalid"]])).to.not.be.ok();
-      expect(key.validation.isDef([function() { /* invalid */}, "name"])).to.not.be.ok();
+      expect(validators.isDef(["name", function() {}])).to.be.ok();
+      expect(validators.isDef([["invalid"]])).to.not.be.ok();
+      expect(validators.isDef([function() { /* invalid */}, "name"])).to.not.be.ok();
     });
   });
   describe('#lib()', function(){
     it('should throw for illegal arguments', function(){
-      expect(key.validation.isLib(["name", function() {}])).to.be.ok();
-      expect(key.validation.isLib(["name", function() {}])).to.be.ok();
-      expect(key.validation.isLib(["invalid"])).to.not.be.ok();
-      expect(key.validation.isLib([function() { /* invalid */}, "name"])).to.not.be.ok();
+      expect(validators.isLib(["name", function() {}])).to.be.ok();
+      expect(validators.isLib(["name", function() {}])).to.be.ok();
+      expect(validators.isLib(["invalid"])).to.not.be.ok();
+      expect(validators.isLib([function() { /* invalid */}, "name"])).to.not.be.ok();
     });
   });
   describe('#run()', function(){
     it('should throw for illegal arguments', function(){
-      expect(key.validation.isRun(["Keyword Name Only"])).to.be.ok();
-      expect(key.validation.isRun(["Keyword Name Only", ["And Args"]])).to.be.ok();
-      expect(key.validation.isRun(["Keyword Name Only", ["And Args And Return"], "=> $return"])).to.be.ok();
+      expect(validators.isRun(["Keyword Name Only"])).to.be.ok();
+      expect(validators.isRun(["Keyword Name Only", ["And Args"]])).to.be.ok();
+      expect(validators.isRun(["Keyword Name Only", ["And Args And Return"], "=> $return"])).to.be.ok();
 
-      expect(key.validation.isRun(["Keyword Name Only", {}])).to.be.ok();
-      expect(key.validation.isRun(["Keyword Name Only", ["And Args"], {}])).to.be.ok();
-      expect(key.validation.isRun(["Keyword Name Only", ["And Args And Return"], "=> $return", {}])).to.be.ok();
+      expect(validators.isRun(["Keyword Name Only", {}])).to.be.ok();
+      expect(validators.isRun(["Keyword Name Only", ["And Args"], {}])).to.be.ok();
+      expect(validators.isRun(["Keyword Name Only", ["And Args And Return"], "=> $return", {}])).to.be.ok();
       
-      expect(key.validation.isRun(["One Key", "Two Keys"])).to.be.ok();
-      expect(key.validation.isRun(["One Key", "=> $oneReturn", "Two Keys", "=> $twoReturn"])).to.be.ok();
-      expect(key.validation.isRun(["One Key", ["One Args"], "Two Keys", ["Two Args"]])).to.be.ok();
-      expect(key.validation.isRun([
+      expect(validators.isRun(["One Key", "Two Keys"])).to.be.ok();
+      expect(validators.isRun(["One Key", "=> $oneReturn", "Two Keys", "=> $twoReturn"])).to.be.ok();
+      expect(validators.isRun(["One Key", ["One Args"], "Two Keys", ["Two Args"]])).to.be.ok();
+      expect(validators.isRun([
         "One Key", ["One Args"], "=> $oneReturn", 
         "Two Keys", ["Two Args"], "=> $twoReturns"
         ])).to.be.ok();
 
-      expect(key.validation.isRun(["Keyword Name Only", ["Args"], ["No args should be here"]])).to.not.be.ok();
-      expect(key.validation.isRun(["Keyword Name Only", "=> $oneReturn", "=> $noReturnHere"])).to.not.be.ok();
-      expect(key.validation.isRun([["No Args Here"], "Keyword Name"])).to.not.be.ok();
-      expect(key.validation.isRun(["=> $noReturnHere", "Keyword Name"])).to.not.be.ok();
+      expect(validators.isRun(["Keyword Name Only", ["Args"], ["No args should be here"]])).to.not.be.ok();
+      expect(validators.isRun(["Keyword Name Only", "=> $oneReturn", "=> $noReturnHere"])).to.not.be.ok();
+      expect(validators.isRun([["No Args Here"], "Keyword Name"])).to.not.be.ok();
+      expect(validators.isRun(["=> $noReturnHere", "Keyword Name"])).to.not.be.ok();
     });
   });
   describe('#isReturn()', function() {
       it("returns true if argument is return statement", function() {
-        expect(key.validation.isReturn("=> $return")).to.be.ok();
-        expect(key.validation.isReturn("=> return")).to.not.be.ok();
-        expect(key.validation.isReturn("=>")).to.not.be.ok();
-        expect(key.validation.isReturn("Something else")).to.not.be.ok();
+        expect(validators.isReturn("=> $return")).to.be.ok();
+        expect(validators.isReturn("=> return")).to.not.be.ok();
+        expect(validators.isReturn("=>")).to.not.be.ok();
+        expect(validators.isReturn("Something else")).to.not.be.ok();
     })
   });
 });
